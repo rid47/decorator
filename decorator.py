fabc31db933132b5b52be6773f345f0f30578241
Md. Ridwan Mizan <ridwanmizan@gmail.com>
2024-10-27 16:13:02 +0600

    decorator done

--- decorator.py
+++ decorator.py
@@ -1,4 +1,5 @@
 import functools
+import time
 # def do_twice(func):
 #     def wrapper_do_twice(*args,**kwargs):
 #         func(*args,**kwargs)
@@ -8,12 +9,45 @@ import functools
 
 
 
-def do_twice(func):
+# def do_twice(func):
+#     @functools.wraps(func)
+#     def wrapper_do_twice(*args, **kwargs):
+#         func(*args, **kwargs)
+#         return func(*args, **kwargs)
+#     return wrapper_do_twice
+
+
+
+
+# def timer(func):
+#     ''' Print the runtime of the decorated function'''
+
+#     @functools.wraps(func)
+#     def wrapper_timer(*args, **kwargs):
+#         start_time = time.perf_counter()
+#         value = func(*args, **kwargs)
+#         end_time = time.perf_counter()
+#         run_time = end_time - start_time
+#         print(f"Finished {func.__name__}() in {run_time:.4f} seconds")
+#         return value
+#     return wrapper_timer
+
+
+
+
+import functools
+
+# ...
+
+def debug(func):
+    """Print the function signature and return value"""
     @functools.wraps(func)
-    def wrapper_do_twice(*args, **kwargs):
-        func(*args, **kwargs)
-        return func(*args, **kwargs)
-    return wrapper_do_twice
-
-
-
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={repr(v)}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__}() returned {repr(value)}")
+        return value
+    return wrapper_debug
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from decorator  import do_twice
+from decorator  import debug
 
 
 # @do_twice
@@ -12,12 +12,42 @@ from decorator  import do_twice
 # say_whee()
 
 
-@do_twice
-def return_greeting(name):
-    print("Creating greeting")
-    return f"Hi {name}"
+# @do_twice
+# def return_greeting(name):
+#     print("Creating greeting")
+#     return f"Hi {name}"
 
-# return_greeting("Ridwan")
+# # return_greeting("Ridwan")
 
-x = return_greeting("Ridwan")
-print(x)
+# x = return_greeting("Ridwan")
+# print(x)
+
+
+### timer decorator test
+
+# @timer
+# def waste_some_time(num_times):
+#     for _ in range(num_times):
+#         sum([number**2 for number in range(10_000)])
+#         # print()
+
+
+# waste_some_time(1)
+# waste_some_time(100)
+
+# for data in range (10_000):
+#     print(data)
+
+
+### debug decorator test
+@debug
+def make_greeting(name,age=None):
+    if age is None:
+        return f"Howdy {name}!"
+    else:
+        return f"Whoa {name}! {age} already, you're growing up!"
+    
+
+
+make_greeting("Ridwan")
+make_greeting("Ridwan",33)
